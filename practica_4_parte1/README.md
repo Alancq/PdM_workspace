014eddf7ce57c07f8791cd3ce9ef68ef9b0feb3d
Alancq <netelmer@gmail.com>
2023-04-08 09:56:50 -0400

    readme

---
+++ practica_4_parte1/README.md
@@ -0,0 +1,26 @@
+Práctica 4 parte 1 
+Autor: E. Alan Cornejo
+Objetivo:Implementar una MEF anti-rebote que permita leer el estado del pulsador de la placa NUCLEO-F429ZI y generar acciones o eventos ante un flanco descendente o ascendente
+El estado inicial de la MEF debe ser BUTTON_UP.
+
+Implementar dentro de main.c, las funciones:
+
+void debounceFSM_init();		// debe cargar el estado inicial
+void debounceFSM_update();	// debe leer las entradas, resolver la lógica de
+					// transición de estados y actualizar las salidas
+void buttonPressed();			// debe invertir el estado del LED1
+void buttonReleased();		// debe invertir el estado del LED3 
+
+El tiempo de anti-rebote debe ser de 40 ms con un retardo no bloqueante como los implementados en la práctica 3.
+
+La función debounceFSM_update() debe llamarse periódicamente.
+
+
+typedef enum{
+BUTTON_UP,
+BUTTON_FALLING,
+BUTTON_DOWN,
+BUTTON_RAISING,
+} debounceState_t
+
+
---
+++ practica_4_parte1/Src/main.c
@@ -0,0 +1,278 @@
+ /**
+  ******************************************************************************
+  * @file    UART/UART_Printf/Src/main.c
+  * @author  MCD Application Team
+  * @brief   This example shows how to retarget the C library printf function
+  *          to the UART.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2017 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+#define DEBOUNCE_TIME 40
+debouceState_t buttonDebounce;
+delay_t AntiRebote;
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+
+
+//delay_t Delays[3];
+int main(void)
+{
+
+
+
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  //MX_GPIO_Init();
+  /* USER CODE BEGIN 2 */
+  BSP_LED_Init(LED1);
+  //BSP_LED_Init(LED2);
+  BSP_LED_Init(LED3);
+  BSP_PB_Init(BUTTON_USER,BUTTON_MODE_GPIO);
+  /* USER CODE END 2 */
+  debounceFSM_init();
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+	  debounceFSM_update();
+
+  }
+  /* USER CODE END 3 */
+}
+void debounceFSM_init()		// debe cargar el estado inicial
+{
+	buttonDebounce=BUTTON_UP;
+	delayInit(&AntiRebote,DEBOUNCE_TIME);
+}
+void debounceFSM_update()	// debe leer las entradas, resolver la lógica de
+					// transición de estados y actualizar las salidas
+{
+	switch(buttonDebounce){
+	case BUTTON_UP:
+		if(BSP_PB_GetState(BUTTON_USER)==true){
+			buttonDebounce=BUTTON_FALLING;
+		}
+		break;
+	case BUTTON_FALLING:
+		if(delayRead(&AntiRebote)==true){
+			if(BSP_PB_GetState(BUTTON_USER)){
+				buttonDebounce=BUTTON_DOWN;
+				buttonPressed();
+			}else{
+				buttonDebounce=BUTTON_UP;
+			}
+		}
+		break;
+	case BUTTON_DOWN:
+		if(!BSP_PB_GetState(BUTTON_USER)==true){
+			buttonDebounce=BUTTON_RAISING;
+		}
+		break;
+	case BUTTON_RAISING:
+		if(delayRead(&AntiRebote)==true){
+			if(!BSP_PB_GetState(BUTTON_USER)){
+				buttonDebounce=BUTTON_UP;
+				buttonReleased();
+			}else{
+				buttonDebounce=BUTTON_DOWN;
+			}
+		}
+		break;
+	}
+}
+void buttonPressed()			// debe invertir el estado del LED1
+{
+	BSP_LED_Toggle(LED1);
+}
+void buttonReleased()		// debe invertir el estado del LED3
+{
+	BSP_LED_Toggle(LED3);
+}
+/**
+  * @brief  System Clock Configuration
+  *         The system Clock is configured as follow : 
+  *            System Clock source            = PLL (HSE)
+  *            SYSCLK(Hz)                     = 180000000
+  *            HCLK(Hz)                       = 180000000
+  *            AHB Prescaler                  = 1
+  *            APB1 Prescaler                 = 4
+  *            APB2 Prescaler                 = 2
+  *            HSE Frequency(Hz)              = 8000000
+  *            PLL_M                          = 8
+  *            PLL_N                          = 360
+  *            PLL_P                          = 2
+  *            PLL_Q                          = 7
+  *            PLL_R                          = 2
+  *            VDD(V)                         = 3.3
+  *            Main regulator output voltage  = Scale1 mode
+  *            Flash Latency(WS)              = 5
+  * @param  None
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+
+  /* Enable Power Control clock */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  
+  /* The voltage scaling allows optimizing the power consumption when the device is 
+     clocked below the maximum system frequency, to update the voltage scaling value 
+     regarding system frequency refer to product datasheet.  */
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+  
+  /* Enable HSE Oscillator and activate PLL with HSE as source */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 360;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 7;
+  if(HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    /* Initialization Error */
+    Error_Handler();
+  }
+  
+  if(HAL_PWREx_EnableOverDrive() != HAL_OK)
+  {
+    /* Initialization Error */
+    Error_Handler();
+  }
+  
+  /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2 
+     clocks dividers */
+  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;  
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;  
+  if(HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+  {
+    /* Initialization Error */
+    Error_Handler();
+  }
+}
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @param  None
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* Turn LED2 on */
+  BSP_LED_On(LED2);
+  while (1)
+  {
+  }
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+  /* Infinite loop */
+  while (1)
+  {
+  }
+}
+#endif
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
